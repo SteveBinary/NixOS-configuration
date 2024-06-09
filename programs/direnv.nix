8ebe30534f4de4ee71b7625856ef1d38521b4427
SteveBinary <SteveBinary@users.noreply.github.com>
2024-06-09 18:31:27 +0200

    Increase direnv warning timeout to not be so annoying.

--- programs/direnv.nix
+++ programs/direnv.nix
@@ -4,7 +4,7 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
-    config.global.warn_timeout = "10s"; # https://github.com/direnv/direnv/blob/master/man/direnv.toml.1.md
+    config.global.warn_timeout = "1h"; # https://github.com/direnv/direnv/blob/master/man/direnv.toml.1.md
   };
 
   home.sessionVariables = {
