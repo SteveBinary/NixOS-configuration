655e161a69e7e0f8a386484111f69c13b41658a0
SteveBinary <SteveBinary@users.noreply.github.com>
2024-06-25 17:04:27 +0200

    Start configuring Oh My Posh with the Catppuccin Mocha theme as a starting point.

--- programs/shells/oh-my-posh.nix
+++ programs/shells/oh-my-posh.nix
@@ -7,47 +7,77 @@
     enableZshIntegration = true;
     settings = {
       version = 2;
+      auto_upgrade = false;
+      disable_notice = true;
       final_space = true;
+      palette = {
+        os = "#ACB0BE";
+        closer = "p:os";
+        pink = "#F5C2E7";
+        lavender = "#B4BEFE";
+        blue = "#89B4FA";
+      };
+      secondary_prompt = {
+        template = " ";
+        foreground = "p:closer";
+      };
       blocks = [
+        {
+          type = "prompt";
+          alignment = "left";
+          segments = [
+            {
+              type = "os";
+              template = "{{.Icon}} ";
+              style = "plain";
+              foreground = "p:os";
+            }
+            {
+              type = "session";
+              template = "{{ .UserName }}@{{ .HostName }} ";
+              style = "plain";
+              foreground = "p:blue";
+            }
+            {
+              type = "path";
+              template = "{{ .Path }}";
+              style = "plain";
+              foreground = "p:pink";
+              properties = {
+                folder_icon = ".. ..";
+                home_icon = "~";
+                style = "agnoster_short";
+              };
+            }
+            {
+              type = "git";
+              style = "plain";
+              foreground = "p:lavender";
+              properties = {
+                branch_icon = " ";
+                cherry_pick_icon = " ";
+                commit_icon = " ";
+                fetch_status = true;
+                fetch_upstream_icon = true;
+                merge_icon = " ";
+                no_commits_icon = " ";
+                rebase_icon = " ";
+                revert_icon = " ";
+                tag_icon = " ";
+              };
+            }
+          ];
+        }
         {
           type = "prompt";
           alignment = "left";
           newline = true;
           segments = [
             {
-              type = "os";
-              style = "diamond";
-              background = "#ffffff";
-              foreground = "#000000";
-              leading_diamond = "";
-              template = "{{ if .WSL }}WSL at {{ end }}{{.Icon}} ";
-            }
-            {
-              background = "#40189c";
-              foreground = "#ffffff";
-              style = "diamond";
-              template = "❘|｜ {{ .UserName }}@{{ .HostName }} ";
-              type = "session";
-            }
-            {
-              background = "#0000ff";
-              foreground = "#000000";
-              powerline_symbol = "";
-              properties.style = "full";
-              style = "powerline";
-              template = "  ";
-              type = "root";
-            }
-            {
-              background = "#0000ff";
-              foreground = "#ffffff";
-              powerline_symbol = "";
-              properties = {
-                style = "full";
-              };
-              style = "powerline";
-              template = " {{ .Path }} ";
-              type = "path";
+              type = "text";
+              template = "";
+              style = "plain";
+              foreground = "p:closer";
             }
           ];
         }
